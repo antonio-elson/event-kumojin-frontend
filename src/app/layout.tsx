f769e58bf2202b5c47d3ee4c4365ec440f8110d7
Antonio Elson <cast.antonio.utic@sebrae.com.br>
2023-07-30 23:18:34 -0300

    feat: configure layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import {
+  ContentFooter,
+  Footer,
+  LinkHeader,
+  LinkNav,
+  Main,
+  Menu,
+  NavHeader,
+} from "./page-styled";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +22,34 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const ItemLinks = () => (
+    <>
+      <LinkHeader href="/about">
+        <p>About</p>
+      </LinkHeader>
+      <LinkHeader href="/events">
+        <p>Créer événement</p>
+      </LinkHeader>
+    </>
+  );
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavHeader>
+          <LinkNav href="/">
+            <p>Events KMJN</p>
+          </LinkNav>
+          <Menu>
+            <ItemLinks />
+          </Menu>
+        </NavHeader>
+        <Main>{children}</Main>
+        <Footer>
+          <ContentFooter>
+            <p>{`by Antonio Elson`}</p>
+          </ContentFooter>
+        </Footer>
+      </body>
     </html>
   );
 }
---
+++ src/app/page-styled.ts
@@ -0,0 +1,69 @@
+"use client";
+import styled from "styled-components";
+
+export const LinkHeader = styled.a`
+  color: #180e8d;
+  margin-left: auto;
+  text-decoration: none;
+`;
+
+export const NavHeader = styled.nav`
+  background: #fff;
+  padding: 22px;
+  display: flex;
+
+  @media (max-width: 768px) {
+    background: #fff;
+    padding: 22px;
+    display: grid;
+  }
+`;
+
+export const LinkNav = styled.a`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  gap: 10;
+  font-size: 24;
+  font-weight: 600;
+`;
+
+export const Main = styled.main`
+  display: grid;
+  height: 80vh;
+  width: 100vw;
+  flex-direction: column;
+  color: #180e8d;
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  margin: 0 20px;
+  align-items: center;
+  margin-left: auto;
+  font-weight: 600;
+  gap: 24px;
+
+  @media (max-width: 768px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+
+export const Footer = styled.footer`
+  background: #fff;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  margin: auto;
+`;
+
+export const ContentFooter = styled.div`
+  display: flex;
+  margin: 0 20px;
+  align-items: center;
+
+  p {
+    color: #c6c2fa;
+  }
+`;
